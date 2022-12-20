fix potential energy computation

src/kernels.py
@@ -57,7 +57,7 @@ class QuinticKernel(BaseKernel):
         
         q4 = np.power(q,4)
         q3 = np.power(q,3)
-        p2 = np.power(q,2)
+        q2 = np.power(q,2)
         
         nabla_W = self.alpha_dim / self.h * (0.625 * q4 - 3.75 * q3 + 7.5 * q2 - 5 * q)
         

src/sph.py
@@ -78,27 +78,27 @@ class BaseSPH(object):
         pass
     
     @abc.abstractmethod
-    def __boundary_check(self):
+    def _boundary_check(self):
         pass
     
-    def __map_neighbour(self):
+    def _map_neighbour(self):
         pass
     
-    def __density_pressure_computation(self):
+    def _density_pressure_computation(self):
         pass
         
-    def __rescale_mass_density_pressure(self):
+    def _rescale_mass_density_pressure(self):
         pass
     
     @abc.abstractmethod
-    def __accel_computation(self):
+    def _accel_computation(self):
         pass
     
     @abc.abstractmethod
-    def __time_stepping(self):
+    def _time_stepping(self):
         pass
     
-    def __energy_computation(self):
+    def _energy_computation(self):
         
         n_dim = self.sim_param.n_dim
         
@@ -115,9 +115,8 @@ class BaseSPH(object):
             
         
         # Ep <- z
-        self.Ep = np.ndarray(self.n_particle, dtype=np.float64, buffer=self.x, 
-                             offset=self.sim_param.n_dim, strides=self.sim_param.n_dim)
-        
+        self.Ep = self.x[self.sim_param.n_dim-1::self.sim_param.n_dim]
+                
         # Energy Computation
         m = self.particle_model.m
         self.Ek = 0.5 * m * self.Ek
@@ -126,6 +125,9 @@ class BaseSPH(object):
         # Total Energy Computation
         self.Ek_total = np.sum(self.Ek)
         self.Ep_total = np.sum(self.Ep)
+        
+        print('Total Kinetic Energy = {}'.format(self.Ek_total))
+        print('Total Potential Energy = {}'.format(self.Ep_total))
         
         
     def clean_up_simulation(self):
@@ -142,25 +144,30 @@ class BasicSPH(BaseSPH):
     
     def run_simulation(self):
         
-
         self.util.init_particle_state(self)
-                
-        # self.__boundary_check()
+        
+        self.util.perturb_particle(self)
+        
+        self.mp_manager.distribute_particle(self)
+
+        self._boundary_check()
 
         fig, ax = plt.subplots()
                 
-        # while (self.sim_param.t < self.sim_param.T - np.finfo(float).eps):
+        while (self.sim_param.t < self.sim_param.T - np.finfo(float).eps):
             
-        #     self.__accel_computation()
-        #     self.__time_stepping()
-        #     self.__boundary_check()
+            self._accel_computation()
+            self._time_stepping()
+            self._boundary_check()
     
-        #     self.sim_param.t += self.sim_param.dt
+            self.sim_param.t += self.sim_param.dt
+            self._energy_computation()
             
-        self.util.plot(self, plt, ax)
+            
+            self.util.plot(self, plt, ax)
             
             
-    def __boundary_check(self):
+    def _boundary_check(self):
         
         # Index
         index = 0
@@ -186,17 +193,19 @@ class BasicSPH(BaseSPH):
                 index += 1
                 
     
-    def __accel_computation(self):
+    def _accel_computation(self):
         
         n_dim = self.sim_param.n_dim
-        shape = self.a.shape
+        shape = self.a.shape[0]
         
-        self.a = 1.0 * np.random.rand(self.n_particle_G * self.sim_param.n_dim) - 0.5
-        self.a[n_dim-1:shape:n_dim] = self.a[n_dim-1:shape:n_dim] - self.atmospheric_model.g
+        self.a = 1.0 * np.random.rand(self.n_particle * self.sim_param.n_dim) - 0.5
 
-    def __time_stepping(self):
+        # self.a[n_dim-1:shape:n_dim] = self.a[n_dim-1:shape:n_dim] - self.atmospheric_model.g
+
+    def _time_stepping(self):
         
         self.v = self.v + self.a * self.sim_param.dt
+        #self.v = 1.0 * np.random.rand(self.n_particle * self.sim_param.n_dim) - 0.5
         self.x = self.x + self.v * self.sim_param.dt
         
         

src/sph_util.py
@@ -3,6 +3,8 @@
 #%% Import
 
 import numpy as np
+import os
+import time
 
 #%% Class Definition
 
@@ -31,18 +33,27 @@ class SPH_Util(object):
         dtype_float = sph.sim_param.float_precision.get_np_dtype()
         dtype_int = sph.sim_param.int_precision.get_np_dtype()
         
-        #%% Assign Position
+        # Assign Position
         
         # Position
         sph.x_G = np.ndarray(shape_2D, dtype = dtype_float, buffer=sph.mp_manager.shm['x_G'].buf)
         
         self.recursion_build(sph, 0, [0 for i in range(n_dim)], n_per_dim, 0)
         
-        #%% Assign 1D and 2D States
-        
+        # Assign 1D States
         sph.id_G = np.ndarray(shape_1D, dtype = dtype_int, buffer=sph.mp_manager.shm['id_G'].buf)
         sph.id_G = np.array([i for i in range(shape_1D)])
+
+        sph.Ek_G = np.ndarray(shape_1D, dtype = dtype_int, buffer=sph.mp_manager.shm['Ek_G'].buf)
+        sph.Ek_G = np.array([0.0 for i in range(shape_1D)])
         
+        sph.Ep_G = np.ndarray(shape_1D, dtype = dtype_int, buffer=sph.mp_manager.shm['Ep_G'].buf)
+        sph.Ep_G = np.array([0.0 for i in range(shape_1D)])
+        
+        sph.E_G = np.ndarray(shape_1D, dtype = dtype_int, buffer=sph.mp_manager.shm['E_G'].buf)
+        sph.E_G = np.array([0.0 for i in range(shape_1D)])
+        
+        # Assign 2D States
         sph.v_G = np.ndarray(shape_2D, dtype = dtype_float, buffer=sph.mp_manager.shm['v_G'].buf)
         sph.v_G = np.array([0.0 for i in range(shape_2D)]) 
         
@@ -54,13 +65,8 @@ class SPH_Util(object):
         
         sph.p_G = np.ndarray(shape_1D, dtype = dtype_float, buffer=sph.mp_manager.shm['p_G'].buf)
         sph.p_G = np.array([1000.0 for i in range(shape_1D)])
-        
-        
-    @staticmethod
-    def perturb_particle(sph):
-        pass
+ 
     
-
     def recursion_build(self, sph, loop_depth, loop_index, loop_lim, i_particle):
         
         while loop_index[loop_depth] < loop_lim[loop_depth]:
@@ -82,8 +88,7 @@ class SPH_Util(object):
                 
             else:
                 
-                i_particle = self.recursion_build(sph, loop_depth + 1, loop_index, loop_lim,
-                                                  i_particle)
+                i_particle = self.recursion_build(sph, loop_depth+1, loop_index, loop_lim, i_particle)
                 
                 
             loop_index[loop_depth] += 1
@@ -92,14 +97,26 @@ class SPH_Util(object):
         loop_index[loop_depth] = 0
         
         return i_particle
-            
-            
+       
+    
+    @staticmethod
+    def perturb_particle(sph):
+        
+        # Define RNG Seed
+        pid = os.getpid() # Process ID
+        current_time = int(time.time()) # Current Time
+        rng = np.random.default_rng(current_time % pid)
+        
+        # Apply Perturbation
+        sph.x_G += 0.001 * rng.random(size=sph.x_G.shape) - 0.0005
+        
+    
     @staticmethod
     def plot(sph, plt, ax):
         
         plt.cla()
-        print(sph.x_G)
-        ax.plot(sph.x_G[::2], sph.x_G[1::2], '.', markersize=10) 
+
+        ax.plot(sph.x[::2], sph.x[1::2], '.', markersize=10) 
         ax.grid(True)
         ax.set_aspect('equal', 'box')
         
@@ -111,4 +128,9 @@ class SPH_Util(object):
         plt.title('$t = {0} \, s$'.format(sph.sim_param.t), usetex=True)
         
         plt.show()
-        plt.pause(0.05)
+        
+        if sph.sim_param.t == 0:
+            plt.pause(1.0)
+        else:
+            plt.pause(0.05)
+                        