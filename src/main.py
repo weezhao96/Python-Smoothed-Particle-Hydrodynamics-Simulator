fixed type system precision conversion error

src/main.py
@@ -2,6 +2,8 @@
 
 #%% Library
 
+import numpy as np
+
 from models import Atmosphere, Particle
 from simulations import SimulationParameter, SimulationDomain
 from kernels import QuinticKernel
@@ -14,23 +16,34 @@ from sph_util import SPH_Util
 
 if __name__ == '__main__':
 
+    #%% Simulation Precision
+    
+    float_precision = FloatType.FLOAT32
+    float_dtype = float_precision.get_np_dtype()
+    
+    int_precision = IntType.INT16
+    int_dtype = int_precision.get_np_dtype()
+
     #%% Model Definition
     
     # Atmosphere
-    earth = Atmosphere(gravitational_strength=9.81)
+    earth = Atmosphere(gravitational_strength=float_dtype(9.81))
     
     # Particle
-    water = Particle(radius_of_influence=0.01,
-                     resting_density=1000.0, viscosity=1.0,
-                     specific_heat_ratio=7.0, speed_of_sound=1480.0)
+    water = Particle(radius_of_influence=float_dtype(0.01),
+                     resting_density=float_dtype(1000.0), viscosity=float_dtype(1.0),
+                     specific_heat_ratio=float_dtype(7.0), speed_of_sound=float_dtype(1480.0))
     
     #%% Simulation Definition
     
-    sim_param = SimulationParameter(n_dim=2, sim_duration=5.0, dt=0.1,
-                                    float_precision=FloatType.FLOAT64, int_precision=IntType.INT16)
+    sim_param = SimulationParameter(n_dim=int_dtype(2),
+                                    sim_duration=float_dtype(5.0), dt=float_dtype(0.1),
+                                    float_precision=float_precision, int_precision=int_precision)
     
-    unit_cube = SimulationDomain(bounding_box=[[0.0,1.0], [0.0,1.0]],
-                                 initial_position=[[0.5,0.6], [0.5,0.6]])
+    unit_cube = SimulationDomain(bounding_box=np.array([[0.0,1.0],
+                                                        [0.0,1.0]], dtype=float_dtype),
+                                 initial_position=np.array([[0.5,0.6],
+                                                            [0.5,0.6]], dtype=float_dtype))
     
     quintiq = QuinticKernel(n_dim=sim_param.n_dim, radius_of_influence=water.h)
     

src/precision_enums.py
@@ -18,19 +18,36 @@ class IntType(Enum):
     INT32 = 4
     INT64 = 8
     
-    def get_np_dtype(self):
+    def get_np_dtype(self, signed=True):
         
-        if self.value == 1:
-            return np.int8
-        
-        elif self.value == 2:
-            return np.int16
-        
-        elif self.value == 4:
-            return np.int32
-        
-        elif self.value == 8:
-            return np.int64 
+        if signed:
+            
+            if self.value == 1:
+                return np.int8
+            
+            elif self.value == 2:
+                return np.int16
+            
+            elif self.value == 4:
+                return np.int32
+            
+            elif self.value == 8:
+                return np.int64
+            
+            
+        else:
+            
+            if self.value == 1:
+                return np.uint8
+            
+            elif self.value == 2:
+                return np.uint16
+            
+            elif self.value == 4:
+                return np.uint32
+            
+            elif self.value == 8:
+                return np.uint64 
     
 
 #%% Float Type

src/sph.py
@@ -88,7 +88,10 @@ class BaseSPH(object):
         pass
         
     def _rescale_mass_density_pressure(self):
-        pass
+        
+        dtype_float = self.sim_param.float_precision.get_np_dtype()
+        
+        self.particle_model.m = dtype_float(self.particle_model.m)
     
     @abc.abstractmethod
     def _accel_computation(self):
@@ -113,10 +116,10 @@ class BaseSPH(object):
             v = self.v[index_start:index_end]
             self.Ek[i] = np.dot(v,v)
             
-        
         # Ep <- z
-        self.Ep = self.x[self.sim_param.n_dim-1::self.sim_param.n_dim]
-                
+        self.Ep = np.array(self.x[self.sim_param.n_dim-1::self.sim_param.n_dim],
+                           dtype=self.sim_param.float_precision.get_np_dtype())
+
         # Energy Computation
         m = self.particle_model.m
         self.Ek = 0.5 * m * self.Ek
@@ -126,8 +129,8 @@ class BaseSPH(object):
         self.Ek_total = np.sum(self.Ek)
         self.Ep_total = np.sum(self.Ep)
         
-        print('Total Kinetic Energy = {}'.format(self.Ek_total))
-        print('Total Potential Energy = {}'.format(self.Ep_total))
+        print('Total Kinetic Energy = {:.3f}'.format(self.Ek_total))
+        print('Total Potential Energy = {:.3f}'.format(self.Ep_total))
         
         
     def clean_up_simulation(self):
@@ -149,22 +152,39 @@ class BasicSPH(BaseSPH):
         self.util.perturb_particle(self)
         
         self.mp_manager.distribute_particle(self)
-
+        
         self._boundary_check()
+        
+        self._rescale_mass_density_pressure()
+        
+        print('t = {:.3f}'.format(self.sim_param.t))
+
+        self._energy_computation()
+
+        print(' ')
 
         fig, ax = plt.subplots()
                 
         while (self.sim_param.t < self.sim_param.T - np.finfo(float).eps):
             
+
             self._accel_computation()
+            
             self._time_stepping()
+
             self._boundary_check()
     
             self.sim_param.t += self.sim_param.dt
+            
+            print('t = {:.3f}'.format(self.sim_param.t))
+
             self._energy_computation()
             
+            print(' ')
+
             
             self.util.plot(self, plt, ax)
+            
             
             
     def _boundary_check(self):
@@ -198,7 +218,7 @@ class BasicSPH(BaseSPH):
         n_dim = self.sim_param.n_dim
         shape = self.a.shape[0]
         
-        self.a = 1.0 * np.random.rand(self.n_particle * self.sim_param.n_dim) - 0.5
+        self.a = 1.0 * np.random.rand(shape).astype(self.sim_param.float_precision.get_np_dtype()) - 0.5
 
         # self.a[n_dim-1:shape:n_dim] = self.a[n_dim-1:shape:n_dim] - self.atmospheric_model.g
 

src/sph_util.py
@@ -31,41 +31,39 @@ class SPH_Util(object):
         
         # Data Type
         dtype_float = sph.sim_param.float_precision.get_np_dtype()
-        dtype_int = sph.sim_param.int_precision.get_np_dtype()
+        dtype_int = sph.sim_param.int_precision.get_np_dtype(signed=False)
         
         # Assign Position
-        
-        # Position
-        sph.x_G = np.ndarray(shape_2D, dtype = dtype_float, buffer=sph.mp_manager.shm['x_G'].buf)
-        
+        sph.x_G = np.ndarray(shape_2D, dtype = dtype_float, buffer=sph.mp_manager.shm['x_G'].buf)        
         self.recursion_build(sph, 0, [0 for i in range(n_dim)], n_per_dim, 0)
         
         # Assign 1D States
-        sph.id_G = np.ndarray(shape_1D, dtype = dtype_int, buffer=sph.mp_manager.shm['id_G'].buf)
-        sph.id_G = np.array([i for i in range(shape_1D)])
+        sph.id_G = np.ndarray(shape_1D, dtype=dtype_int, buffer=sph.mp_manager.shm['id_G'].buf)
+        sph.id_G = np.array([i for i in range(shape_1D)], dtype=dtype_int)
 
-        sph.Ek_G = np.ndarray(shape_1D, dtype = dtype_int, buffer=sph.mp_manager.shm['Ek_G'].buf)
-        sph.Ek_G = np.array([0.0 for i in range(shape_1D)])
+        sph.Ek_G = np.ndarray(shape_1D, dtype=dtype_float, buffer=sph.mp_manager.shm['Ek_G'].buf)
+        sph.Ek_G = np.zeros(shape_1D, dtype=dtype_float)
         
-        sph.Ep_G = np.ndarray(shape_1D, dtype = dtype_int, buffer=sph.mp_manager.shm['Ep_G'].buf)
-        sph.Ep_G = np.array([0.0 for i in range(shape_1D)])
+        sph.Ep_G = np.ndarray(shape_1D, dtype=dtype_float, buffer=sph.mp_manager.shm['Ep_G'].buf)
+        sph.Ep_G = np.zeros(shape_1D, dtype=dtype_float)
         
-        sph.E_G = np.ndarray(shape_1D, dtype = dtype_int, buffer=sph.mp_manager.shm['E_G'].buf)
-        sph.E_G = np.array([0.0 for i in range(shape_1D)])
+        sph.E_G = np.ndarray(shape_1D, dtype=dtype_float, buffer=sph.mp_manager.shm['E_G'].buf)
+        sph.E_G = np.zeros(shape_1D, dtype=dtype_float)
         
+        sph.rho_G = np.ndarray(shape_1D, dtype=dtype_float, buffer=sph.mp_manager.shm['rho_G'].buf)
+        sph.rho_G = np.array([dtype_float(sph.particle_model.rho_0) for i in range(shape_1D)],
+                             dtype=dtype_float)
+        
+        sph.p_G = np.ndarray(shape_1D, dtype=dtype_float, buffer=sph.mp_manager.shm['p_G'].buf)
+        sph.p_G = np.zeros(shape_1D, dtype=dtype_float)
+                
         # Assign 2D States
-        sph.v_G = np.ndarray(shape_2D, dtype = dtype_float, buffer=sph.mp_manager.shm['v_G'].buf)
-        sph.v_G = np.array([0.0 for i in range(shape_2D)]) 
+        sph.v_G = np.ndarray(shape_2D, dtype=dtype_float, buffer=sph.mp_manager.shm['v_G'].buf)
+        sph.v_G = np.zeros(shape_2D, dtype=dtype_float)
         
-        sph.a_G = np.ndarray(shape_2D, dtype = dtype_float, buffer=sph.mp_manager.shm['a_G'].buf)
-        sph.a_G = np.array([0.0 for i in range(shape_2D)])
+        sph.a_G = np.ndarray(shape_2D, dtype=dtype_float, buffer=sph.mp_manager.shm['a_G'].buf)
+        sph.a_G = np.zeros(shape_2D, dtype=dtype_float)
         
-        sph.rho_G = np.ndarray(shape_1D, dtype = dtype_float, buffer=sph.mp_manager.shm['rho_G'].buf)
-        sph.rho_G = np.array([sph.particle_model.rho_0 for i in range(shape_1D)])
-        
-        sph.p_G = np.ndarray(shape_1D, dtype = dtype_float, buffer=sph.mp_manager.shm['p_G'].buf)
-        sph.p_G = np.array([1000.0 for i in range(shape_1D)])
- 
     
     def recursion_build(self, sph, loop_depth, loop_index, loop_lim, i_particle):
         
@@ -120,12 +118,12 @@ class SPH_Util(object):
         ax.grid(True)
         ax.set_aspect('equal', 'box')
         
-        plt.xlabel('$x$', fontsize=15, usetex=True)
-        plt.ylabel('$y$', fontsize=15, usetex=True)
+        plt.xlabel('$x$', fontsize=15, usetex=False)
+        plt.ylabel('$y$', fontsize=15, usetex=False)
         plt.xlim(sph.sim_domain.bounding_box[0])
         plt.ylim(sph.sim_domain.bounding_box[1])
         
-        plt.title('$t = {0} \, s$'.format(sph.sim_param.t), usetex=True)
+        plt.title('$t = {0:.3f} \, s$'.format(sph.sim_param.t), usetex=False)
         
         plt.show()
         