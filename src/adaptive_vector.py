d2fcfbdf82f334426014c8710997b38bfe20ff24
zhao <weezhao@live.com>
2023-03-23 16:08:57 +0800

    introduce nd_array field to AdaptiveVector

--- src/adaptive_vector.py
+++ src/adaptive_vector.py
@@ -9,6 +9,7 @@ import numpy.typing as npt
 
 class AdaptiveVector(object):
 
+    nd_array: int
     n_dim: int
     n_particle: int
     shape: tuple[int]
@@ -18,12 +19,13 @@ class AdaptiveVector(object):
     end_chunk_index: int
     gap_chunk_index: list[int]
 
-    def __init__(self, n_dim: int, dtype: npt.DTypeLike):
+    def __init__(self, nd_array: int, n_dim: int, dtype: npt.DTypeLike):
         
+        self.nd_array = nd_array
         self.n_dim = n_dim
         self.n_particle = 0
         self.shape = tuple([0])
-        self.end_index = self.n_particle * self.n_dim
+        self.end_index = self.n_particle * self.n_dim if self.nd_array == 2 else self.n_particle
 
         self.data = np.ndarray(shape=self.shape, dtype=dtype)
         self.end_chunk_index = 0
@@ -41,20 +43,20 @@ class AdaptiveVector(object):
         self.data.resize(shape)
 
 
-    def append_data(self, data: np.ndarray):
+    def append_data(self, new_data: np.ndarray):
 
-        size: int = data.shape[0] // self.n_dim
+        size: int = new_data.shape[0] // self.n_dim if self.nd_array == 2 else new_data.shape[0]
 
-        start_index: int = self.n_particle * self.n_dim
-        end_index: int = start_index + data.shape[0]
+        start_index: int = self.end_index
+        end_index: int = start_index + new_data.shape[0]
 
         if (self.shape[0] < end_index):
             self._resize_shape(end_index - self.shape[0] + 10)
 
-        self.data[start_index : end_index] = data[:]
+        self.data[start_index : end_index] = new_data[:]
 
         self.n_particle += size
-        self.end_index = self.n_particle * self.n_dim
+        self.end_index = self.n_particle * self.n_dim if self.nd_array == 2 else self.n_particle
         self.end_chunk_index = self.n_particle
 
     
@@ -85,5 +87,5 @@ class AdaptiveVector(object):
 
             self.gap_chunk_index = list(filter(lambda x: x <= self.end_chunk_index, self.gap_chunk_index))
 
-        self.end_index = self.n_particle * self.n_dim
+        self.end_index = self.n_particle * self.n_dim if self.nd_array == 2 else self.n_particle
         self.gap_chunk_index.clear()
